1006d89adaff0811b699ce7dd24200b48e998426
trevor <trevor@crowdhubapps.com>
2024-11-11 10:44:33 -0800

    base

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,18 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-    },
-    resolve: {
-      alias: {
-        app: resolve(__dirname, "src", "app"),
-        components: resolve(__dirname, "src", "components"),
-        hooks: resolve(__dirname, "src", "hooks"),
-      },
-    },
-  };
+	return {
+		plugins: [react()],
+		server: {
+			port: 3000,
+		},
+		base: "https://trevorcfarr.github.io/n-robitaille",
+		resolve: {
+			alias: {
+				app: resolve(__dirname, "src", "app"),
+				components: resolve(__dirname, "src", "components"),
+				hooks: resolve(__dirname, "src", "hooks"),
+			},
+		},
+	};
 });
