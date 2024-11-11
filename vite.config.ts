cea723289a4a9e02093ceae4834e755b3997ae2b
trevor <trevor@crowdhubapps.com>
2024-11-11 10:49:37 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(() => {
 		server: {
 			port: 3000,
 		},
-		base: "https://trevorcfarr.github.io/n-robitaille",
+		base: ".",
 		resolve: {
 			alias: {
 				app: resolve(__dirname, "src", "app"),
